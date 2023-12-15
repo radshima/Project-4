740c5a08b59a29ae2931493948c564cf980d9952
radshima <shima.akbarirad@gmail.com>
2023-12-16 10:03:14 +1100

    filter added

--- foods/models.py
+++ foods/models.py
@@ -3,6 +3,11 @@ from datetime import date
 
 # Create your models here.
 class Food(models.Model):
+  FOOD_TYPE = (
+  ('drinks', 'Drinks'),
+  ('dinner', 'Dinner'),
+  ('lunch', 'Lunch'),
+)
   name = models.CharField(max_length=100)
   description = models.CharField(max_length=50)
   rate = models.IntegerField(default=0)
@@ -10,6 +15,7 @@ class Food(models.Model):
   time = models.IntegerField()
   pub_date = models.DateField(auto_now=False, auto_now_add=True)
   photo = models.ImageField(upload_to='foods/')
+  type_food = models.CharField(max_length=10,choices=FOOD_TYPE,default="drinks")
   
   def __str__(self):
     return self.name
